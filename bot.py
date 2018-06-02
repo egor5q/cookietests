cbed3f313da9ace4e5c427154b4bffa1c1cc5170
egor5q <31898567+egor5q@users.noreply.github.com>
2018-06-02 15:05:04 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -991,6 +991,7 @@ def results(id):
             if games[id]['bots'][ids]['die']!=1:
                 if games[id]['bots'][ids]['id']<0:
                   games[id]['bots'][ids]['id']-=(games[id]['bots'][ids]['id']*2)
+                  games[id]['bots'][ids]['name']=games[id]['bots'][ids]['id']['name']
                   print(games[id]['bots'][ids]['id'])
                 name=games[id]['bots'][ids]['name']
                 winner=games[id]['bots'][ids]
